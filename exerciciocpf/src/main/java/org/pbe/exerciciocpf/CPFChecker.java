ed86dd8e20df4b4c3a50692f5e1ddf4461cc1da7
MarcusJhuan <marcus.jel05@aluno.ifsc.edu.br>
2026-02-19 20:07:37 -0300

    Att CpfChecker

--- exerciciocpf/src/main/java/org/pbe/exerciciocpf/CPFChecker.java
+++ exerciciocpf/src/main/java/org/pbe/exerciciocpf/CPFChecker.java
@@ -1,18 +1,29 @@
 package org.pbe.exerciciocpf;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.security.InvalidParameterException;
+import java.util.Random;
 
 @RestController
 public class CPFChecker {
 
     @GetMapping("/api/verificador/{cpf}")
-    public String check_in_uri(@PathVariable String cpf) {
-        try {
-            CPF check = new CPF(cpf);
-                    return "OK";
-        } catch (InvalidParameterException e) {
-            return "INVALIDO";
-        }
+    public ResponseEntity<String> check_in_uri(@PathVariable String cpf) {
+       Random r = new Random();
+       if (r.nextBoolean()){
+           return ResponseEntity.ok().body("OK");
+       }
+        return ResponseEntity.badRequest().body("INVÁLIDO");
+
+
+
+
+       //        try {
+//            CPF check = new CPF(cpf);
+//                    return ResponseEntity.ok().body("OK");
+//        } catch (InvalidParameterException e) {
+//            return ResponseEntity.badRequest().body("INVÁLIDO");
+//        }
     }
     @GetMapping("/api/verificador")
     public String check_in_query(@RequestParam String cpf) {
